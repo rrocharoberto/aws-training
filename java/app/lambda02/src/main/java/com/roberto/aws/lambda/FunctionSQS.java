aa756ea9ae378ee0d132e571abe88b97d2ac523b
Roberto Rocha <rrocha.roberto@gmail.com>
2022-11-01 09:55:03 -0300

    Create architecture diagram

--- java/app/lambda02/src/main/java/com/roberto/aws/lambda/FunctionSQS.java
+++ java/app/lambda02/src/main/java/com/roberto/aws/lambda/FunctionSQS.java
@@ -5,15 +5,20 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
 
-public class FunctionSQS implements RequestHandler<SQSEvent, Void>{
+public class FunctionSQS implements RequestHandler<SQSEvent, String> {
 
     @Override
-    public Void handleRequest(SQSEvent event, Context context) {
+    public String handleRequest(SQSEvent event, Context context) {
         context.getLogger().log("Event received in SQL Lambda: " + event);
-        for(SQSMessage msg : event.getRecords()){
+        for (SQSMessage msg : event.getRecords()) {
             context.getLogger().log("Event body " + new String(msg.getBody()));
+
+            // Checks if the message should raise an error -> send to DLQ
+            if (msg.getBody().contains("Error")) {
+                context.getLogger().log("Raising error. This will trigger a send message to DLQ");
+                throw new RuntimeException("Invalid message input.");
+            }
         }
-        return null;
+        return "SUCCESS";
     }
 }
-
