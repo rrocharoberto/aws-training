c036d53717e51e43b1aa574b8d64d0220d8a3333
Roberto Rocha <rrocha.roberto@gmail.com>
2022-11-02 22:34:03 -0300

    Add infra for lambda05

--- java/infra/main.tf
+++ java/infra/main.tf
@@ -6,11 +6,13 @@ locals {
   lambda02_source_file = "${path.module}/../app/lambda02/target/lambda02-0.1.jar"
   lambda03_source_file = "${path.module}/../app/lambda03/target/lambda03-0.1.jar"
   lambda04_source_file = "${path.module}/../app/lambda04/target/lambda04-0.1.jar"
+  lambda05_source_file = "${path.module}/../app/lambda05/target/lambda05-0.1.jar"
 
   lambda01_class_name = "com.roberto.aws.lambda.FunctionHello"
   lambda02_class_name = "com.roberto.aws.lambda.FunctionSQSConsumer"
   lambda03_class_name = "com.roberto.aws.lambda.FunctionSQSProducer"
-  lambda04_class_name = "com.roberto.aws.lambda.FunctionDynamoEvent"
+  lambda04_class_name = "com.roberto.aws.lambda.FunctionDLQConsumer"
+  lambda05_class_name = "com.roberto.aws.lambda.FunctionDynamoEvent"
 }
 
 terraform {
@@ -50,7 +52,7 @@ module "lambda_sqs" {
   lab_role_arn       = var.lab_role_arn
 }
 
-module "lambda_dlq" {
+module "lambda_event_generator" {
   source             = "./modules/lambda"
   base_name          = "03-${local.base_name}"
   lambda_source_file = local.lambda03_source_file
@@ -59,11 +61,20 @@ module "lambda_dlq" {
   lab_role_arn       = var.lab_role_arn
 }
 
-module "lambda_dynamo_source" {
+module "lambda_dlq" {
   source             = "./modules/lambda"
   base_name          = "04-${local.base_name}"
   lambda_source_file = local.lambda04_source_file
   lambda_class_name  = local.lambda04_class_name
+  aws_region         = var.aws_region
+  lab_role_arn       = var.lab_role_arn
+}
+
+module "lambda_dynamoDB_stream" {
+  source             = "./modules/lambda"
+  base_name          = "05-${local.base_name}"
+  lambda_source_file = local.lambda05_source_file
+  lambda_class_name  = local.lambda05_class_name
   aws_region         = var.aws_region
   lab_role_arn       = var.lab_role_arn
 }
@@ -79,7 +90,7 @@ module "sqs" {
 module "dlq" {
   source                 = "./modules/sqs/dlq"
   base_name              = local.base_name
-  destination_lambda_arn = module.lambda_dynamo_source.lambda_function_arn
+  destination_lambda_arn = module.lambda_dlq.lambda_function_arn
   lab_role_arn           = var.lab_role_arn
 }
 
