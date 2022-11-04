64aa8bb4fd75a4e2de8c66d1b5e1e2bac154be8d
Roberto Rocha <rrocha.roberto@gmail.com>
2022-11-04 14:08:52 -0300

    Test logging configuration

--- java/app/lambda01/src/main/java/com/roberto/aws/lambda/FunctionHello.java
+++ java/app/lambda01/src/main/java/com/roberto/aws/lambda/FunctionHello.java
@@ -3,11 +3,18 @@ package com.roberto.aws.lambda;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class FunctionHello implements RequestHandler<String, String> {
     
+    private static final Logger logger = LoggerFactory.getLogger(FunctionHello.class);
+
     @Override
     public String handleRequest(String inMessage, Context context) {
         context.getLogger().log("Message received in lambda: " + inMessage);
+
+        logger.info("slf4j logger ok.");
 
         return "Hello " + inMessage + " from " + context.getFunctionName();
     }
