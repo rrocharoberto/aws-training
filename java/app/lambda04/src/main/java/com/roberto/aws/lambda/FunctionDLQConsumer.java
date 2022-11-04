ed6ec4e0eebd900feba533fb43910d8dbe1ef84b
Roberto Rocha <rrocha.roberto@gmail.com>
2022-11-04 14:13:13 -0300

    Fix lambda 4 logic.

--- java/app/lambda04/src/main/java/com/roberto/aws/lambda/FunctionDLQConsumer.java
+++ java/app/lambda04/src/main/java/com/roberto/aws/lambda/FunctionDLQConsumer.java
@@ -14,15 +14,9 @@ public class FunctionDLQConsumer implements RequestHandler<SQSEvent, String> {
 
   @Override
   public String handleRequest(SQSEvent event, Context context) {
-    logger.info("Event received in SQS Lambda: " + event);
+    logger.info("Event received in Lambda: " + event);
     for (SQSMessage msg : event.getRecords()) {
-      logger.info("Event body " + new String(msg.getBody()));
-
-      // Checks if the message should raise an error -> send to DLQ
-      if (msg.getBody().contains("Error")) {
-        logger.info("Raising error. This will send the message to DLQ");
-        throw new RuntimeException("Invalid message input.");
-      }
+      logger.info("Processing message:" + msg.getBody());
     }
     return "SUCCESS";
   }
