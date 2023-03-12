310242aa4347690397e4cf0f44a5f5134b89e808
Roberto Rocha <rrocha.roberto@gmail.com>
2023-03-12 15:02:47 -0300

    Add log and raise exception

--- overview-01/app/lambda-01/lambda_function.py
+++ overview-01/app/lambda-01/lambda_function.py
@@ -2,8 +2,12 @@ import boto3
 import os
 
 def lambda_handler(event, context):
+    print("Running lambda with input data: " + str(event))
     print("Environment: " + os.environ["ENVIRONMENT"])
     print("Service: " + os.environ["SERVICE"])
+    if "error" in event:
+        raise Exception(event["error"])
+
     result = "Hello World: " + str(event)
     return {
         'statusCode' : 200,
