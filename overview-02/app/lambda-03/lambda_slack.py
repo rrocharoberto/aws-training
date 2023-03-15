b8cbae8170305de8ebd6d766cbc0cd680ee02969
Roberto Rocha <rrocha.roberto@gmail.com>
2023-03-15 19:06:17 -0300

    improve slack message

--- overview-02/app/lambda-03/lambda_slack.py
+++ overview-02/app/lambda-03/lambda_slack.py
@@ -14,9 +14,9 @@ def lambda_handler(event, context):
   msg = {
     "channel": os.environ["SLACK_CHANNEL"],
     "username": "aws-alarm",
-    "text": event['Records'][0]['Sns']['Subject'] 
-      + "\nstate: " + parsed_message['NewStateValue']
-      + "\nreason: " + parsed_message['NewStateReason']
+    "text": "\nAlarm: " + parsed_message['AlarmName']
+      + "\nState: " + parsed_message['NewStateValue']
+      + "\nReason: " + parsed_message['NewStateReason']
   }
   encoded_msg = json.dumps(msg).encode('utf-8')
   resp = http.request('POST', url, body=encoded_msg)
